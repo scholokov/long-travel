360a2e00579ed18b8e8340f5334bc17cb01f052e
Denis Shcholokov <d.shcholokov@tsintegr.com>
2018-08-01 17:13:23 +0300

    adding Lviv

---
+++ Travels/20070406_09_Lviv/article.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+    <HEAD>
+        <TITLE></TITLE>
+    </HEAD>
+    <BODY>
+        <article>
+            <header>
+                <?php include 'article header.html' ?>
+            </header>
+            <article class="article-body">
+                <?php
+                    include 'article_01_Hotel.html'    ;
+                    include 'article_02_Lviv.html'                        ;
+                    include 'article_03_Lviv_Area.html'                                   ;
+                    include 'article_04_Road.html'                                      ;
+                ?>
+        </article>
+        </article>
+    </BODY>
+</HTML>
