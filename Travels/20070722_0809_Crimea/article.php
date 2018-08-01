6c2616505505153e14476998f94398ca3457d317
Denis Shcholokov <d.shcholokov@tsintegr.com>
2018-08-01 17:32:57 +0300

    adding

---
+++ Travels/20070722_0809_Crimea/article.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+    <HEAD>
+        <TITLE></TITLE>
+    </HEAD>
+    <BODY>
+        <article>
+            <header>
+                <?php include 'article header.html' ?>
+            </header>
+			<article>
+                <?php include 'article_01_Road.html' ?>
+                <?php include 'article_02_Ribache.html' ?>
+                <?php include 'article_03_Beach.html' ?>
+                <?php include 'article_04_Hotel.html' ?>
+                <?php include 'article_05_Road.html' ?>
+                <?php include 'article_06_Balaklava.html' ?>
+                <?php include 'article_07_Balaklava_Museum.html' ?>
+                <?php include 'article_08_Sevastopol.html' ?>
+                <?php include 'article_09_Sudak.html' ?>
+                <?php include 'article_10_Noviy_Svet.html' ?>
+                <?php include 'article_11_Yalta.html' ?>
+                <?php include 'article_12_Road.html' ?>
+                <?php include 'article_13_After_Road.html' ?>
+			</article>
+        </article>
+    </BODY>
+</HTML>
