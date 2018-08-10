e2e7278d5e7a439a316fcb83ce041c110778dc40
Denis Shcholokov <d.shcholokov@tsintegr.com>
2018-08-10 17:50:08 +0300

    20150502_06_Hungary

---
+++ Travels/20150502_06_Hungary/article.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+    <HEAD>
+        <TITLE></TITLE>
+    </HEAD>
+    <BODY>
+        <article>
+            <header>
+                <?php include 'article header.html' ?>
+            </header>
+			<article>
+                <?php include 'article_01_Road.html' ?>
+                <?php include 'article_02_Hotel.html' ?>
+                <?php include 'article_03_Debrecen.html' ?>
+                <?php include 'article_04_Debrecen_Zoo.html' ?>
+                <?php include 'article_05_Road.html' ?>
+                <?php include 'article_06_Leanka.html' ?>
+                <?php include 'article_07_Hotel.html' ?>
+                <?php include 'article_08_Road.html' ?>
+			</article>
+        </article>
+    </BODY>
+</HTML>
