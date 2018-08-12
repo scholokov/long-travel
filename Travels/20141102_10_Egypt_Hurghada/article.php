88c3e5470a8e75c20628b2340c5e5412bbe0a24f
Denis Scholokov <scholokov@users.noreply.github.com>
2018-08-12 17:15:03 +0300

    init

---
+++ Travels/20141102_10_Egypt_Hurghada/article.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+    <HEAD>
+        <TITLE></TITLE>
+    </HEAD>
+    <BODY>
+        <article>
+            <header>
+                <?php include 'article header.html' ?>
+            </header>
+			<article>
+                <?php include 'article_01_Hotel.html' ?>
+                <?php include 'article_02_Beach.html' ?>
+                <?php include 'article_03_1000_1.html' ?>
+                <?php include 'article_04_El_Gouna.html' ?>
+                <?php include 'article_05_Synbad.html' ?>
+                <?php include 'article_06_Road.html' ?>
+			</article>
+        </article>
+    </BODY>
+</HTML>
