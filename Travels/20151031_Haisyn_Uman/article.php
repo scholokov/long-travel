d2042719d63d40bb88068080235ade8bd9b48f2e
Denis Scholokov <scholokov@users.noreply.github.com>
2018-08-12 16:37:32 +0300

    haysyn

---
+++ Travels/20151031_Haisyn_Uman/article.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+    <HEAD>
+        <TITLE></TITLE>
+    </HEAD>
+    <BODY>
+        <article>
+            <header>
+                <?php include 'article header.html' ?>
+            </header>
+            <article class="article-body">
+                <?php
+                    include 'article_01_Sport.html'                         ;
+                    include 'article_02_Mіnі_Gotel_Pozitiv.html'            ;
+                    include 'article_03_Sofiivka.html'                      ;
+                ?>
+        </article>
+        </article>
+    </BODY>
+</HTML>
