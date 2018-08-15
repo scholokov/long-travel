7df6a39378268b7775a57e9967d882911b7094e9
Denis Scholokov <scholokov@users.noreply.github.com>
2018-08-15 10:44:44 +0300

    init

---
+++ Travels/20170513_Putivl_Kruti/article.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+    <HEAD>
+        <TITLE></TITLE>
+    </HEAD>
+    <BODY>
+        <article>
+            <header>
+                <?php include 'article header.html' ?>
+            </header>
+			<article>
+                <?php include 'article_01_Road.html' ?>
+                <?php include 'article_02.html' ?>
+                <?php include 'article_03_Road.html' ?>
+                <?php include 'article_04.html' ?>
+                <?php include 'article_05_Road.html' ?>
+			</article>
+        </article>
+    </BODY>
+</HTML>
