df346854bfe9b885419e551b3618ba15ca170267
Denis Scholokov <scholokov@users.noreply.github.com>
2018-08-16 23:29:21 +0300

    init

---
+++ Travels/20091026_1102_Toronto/article.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+    <HEAD>
+        <TITLE></TITLE>
+    </HEAD>
+    <BODY>
+        <article>
+            <header>
+                <?php include 'article header.html' ?>
+            </header>
+            <article class="article-body">
+                <?php
+                    include 'article_01_Hotel.html'                           ;
+                    include 'article_02_City.html'                      ;
+                    include 'article_03_Tower.html'                             ;
+                    include 'article_04_Parad.html'                              ;
+                ?>
+        </article>
+    </BODY>
+</HTML>
