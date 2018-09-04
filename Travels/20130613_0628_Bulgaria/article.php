1efdd1d4b62f3301c26cd1556521479196876a46
Denis Scholokov <scholokov@users.noreply.github.com>
2018-09-04 20:24:43 +0300

    init

---
+++ Travels/20130613_0628_Bulgaria/article.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+    <HEAD>
+        <TITLE></TITLE>
+    </HEAD>
+    <BODY>
+        <article>
+            <header>
+                <?php include 'article header.html' ?>
+            </header>
+			<article>
+                <?php include 'article_01_Road.html' ?>
+                <?php include 'article_02_Hotel_Inzov_Bolgrad.html' ?>
+                <?php include 'article_03_Road.html' ?>
+                <?php include 'article_04_Excelsior_Bolgaria.html' ?>
+                <?php include 'article_05_Beach_Bolgaria.html' ?>
+                <?php include 'article_06_WaterPool_Bolgaria.html' ?>
+                <?php include 'article_07_City_Bolgaria.html' ?>
+                <?php include 'article_08_Presents_Bolgaria.html' ?>
+                <?php include 'article_09_Road.html' ?>
+                <?php include 'article_10_Hotel_Bucharest.html' ?>
+                <?php include 'article_11_Bucharest_Rimania.html' ?>
+                <?php include 'article_12_Road.html' ?>
+                <?php include 'article_13_Hotel_Slanic.html' ?>
+                <?php include 'article_14_Slanic.html' ?>
+                <?php include 'article_15_Road.html' ?>
+                <?php include 'article_16_Hotel_Savran.html' ?>
+                <?php include 'article_17_Road.html' ?>
+			</article>
+        </article>
+    </BODY>
+</HTML>
