21a3f114f90385920ef21877245d379b7aced26b
Denis Scholokov <scholokov@users.noreply.github.com>
2018-09-07 21:24:33 +0300

    init

---
+++ Travels/20140616_0707_Greece/article.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+    <HEAD>
+        <TITLE></TITLE>
+    </HEAD>
+    <BODY>
+        <article>
+            <header>
+                <?php include 'article header.html' ?>
+            </header>
+			<article>
+                <?php include 'article_01_Road.html' ?>
+                <?php include 'article_02_Hotel.html' ?>
+                <?php include 'article_03_Road.html' ?>
+                <?php include 'article_04_Hotel.html' ?>
+                <?php include 'article_05_Budapest.html' ?>
+                <?php include 'article_06_Budapest_Zoo.html' ?>
+                <?php include 'article_07_Road.html' ?>
+                <?php include 'article_08_Hotel.html' ?>
+                <?php include 'article_09_Zitorade.html' ?>
+                <?php include 'article_10_Serbia.html' ?>
+                <?php include 'article_11_Road.html' ?>
+                <?php include 'article_12_Hotel.html' ?>
+                <?php include 'article_13_Beach.html' ?>
+                <?php include 'article_14_Greece.html' ?>
+                <?php include 'article_15_Road.html' ?>
+                <?php include 'article_16_Hotel.html' ?>
+                <?php include 'article_17_Ruse.html' ?>
+                <?php include 'article_18_Road.html' ?>
+                <?php include 'article_19_Transfagarasan_Road.html' ?>
+                <?php include 'article_20_Hotel.html' ?>
+                <?php include 'article_21_Road.html' ?>
+                <?php include 'article_22_Hotel.html' ?>
+                <?php include 'article_24_Zoo.html' ?>
+                <?php include 'article_25_Road.html' ?>
+			</article>
+        </article>
+    </BODY>
+</HTML>
