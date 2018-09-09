cf9dbf2f345b10ecb87c4180cba53d6b8701abe1
Denis Scholokov <scholokov@users.noreply.github.com>
2018-09-09 11:28:32 +0300

    init

---
+++ Travels/20160701_24_Italy/article.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+    <HEAD>
+        <TITLE></TITLE>
+    </HEAD>
+    <BODY>
+        <article>
+            <header>
+                <?php include 'article header.html' ?>
+            </header>
+			<article class="article-body">
+                <?php
+                    include 'article_00_Preparation_10.07.html'                             ;
+                    include 'article_01_Road_Home_10.07.html'                               ;
+                    include 'article_02_Palma_10.07-11.07.html'                             ;
+                    include 'article_03_Hungary_-_Budapest_11.07.html'                      ;
+                    include 'article_031_Road_-_Hungary_-_Austria_11.07.html'               ;
+                    include 'article_04_Austria_11.07-13.07.html'                           ;
+                    include 'article_041_Austria_Hotel_11.07-13.07.html'                    ;
+                    include 'article_042_Austria_Drobollach_11.07-13.07.html'               ;
+                    include 'article_05_Road_13.07.html'                                    ;
+                    include 'article_06_Italy_13.07-20.07.html'                             ;
+                    include 'article_061_Other.html'                                        ;
+                    include 'article_062_Beach.html'                                        ;
+                    include 'article_063_San_Marino.html'                                   ;
+                    include 'article_064_Rimini_Miniatures.html'                            ;
+                    include 'article_065_Residence_Domus,_Rivazzurra,_Rimini,_Italy.html'   ;
+                    include 'article_07_Road_20.07.html'                                    ;
+                    include 'article_08_Germany_20.07-22.07.html'                           ;
+                    include 'article_081_Ringhotel_Die_Gams,_Beilngries,_Germany.html'      ;
+                    include 'article_09_Road_22.07.html'                                    ;
+                    include 'article_10_Poland_22.07-23.07.html'                            ;
+                    include 'article_101_Hotel_Vanilla,_Krakow,_Poland.html'                ;
+                    include 'article_121_Lviv_Kopa.html'                                    ;
+                    include 'article_11_Road_23.07.html'                                    ;
+                    include 'article_12_Lviv_23.07-24.07.html'                              ;
+                    include 'article_13_Road_24.07.html'                                    ;
+                ?>
+			</article>
+        </article>
+    </BODY>
+</HTML>
