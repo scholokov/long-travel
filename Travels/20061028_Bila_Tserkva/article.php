94b21ff73d3caf82438123530760acd529a18002
Denis Scholokov <scholokov@users.noreply.github.com>
2018-09-09 12:18:08 +0300

    sidebar

--- Travels/20061028_Bila_Tserkva/article.php
+++ Travels/20061028_Bila_Tserkva/article.php
@@ -5,9 +5,11 @@
     <BODY>
         <article>
             <header>
+                <a name="header"></a>
                 <?php include 'article header.html' ?>
             </header>
 			<article>
+                <a name="article"></a>
                 <?php include 'article.html' ?>
 			</article>
         </article>
