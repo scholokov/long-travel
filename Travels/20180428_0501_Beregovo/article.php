2079f7cf311a932903c308b13a445230914b4251
Denis Shcholokov <d.shcholokov@tsintegr.com>
2018-09-14 14:26:25 +0300

    beregovo

---
+++ Travels/20180428_0501_Beregovo/article.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+    <HEAD>
+        <TITLE></TITLE>
+    </HEAD>
+    <BODY>
+        <article>
+            <header>
+                <a name="header"></a><?php include 'article header.html' ?>
+            </header>
+			<article>
+                <a name="article_01_Road"><?php include 'article_01_Road.html' ?>
+                <a name="article_02_Cafee"><?php include 'article_02_Cafee.html' ?>
+                <a name="article_03_Road"><?php include 'article_03_Road.html' ?>
+                <a name="article_04_Hotel"><?php include 'article_04_Hotel.html' ?>
+                <a name="article_05_Leanka"><?php include 'article_05_Leanka.html' ?>
+                <a name="article_06_Uzhgorod"><?php include 'article_06_Uzhgorod.html' ?>
+                <a name="article_07_Cafee"><?php include 'article_07_Cafee.html' ?>
+                <a name="article_08_Mukachevo"><?php include 'article_08_Mukachevo.html' ?>
+                <a name="article_09_Zamok"><?php include 'article_09_Zamok.html' ?>
+                <a name="article_10_Sanatoriy_Karpaty"><?php include 'article_10_Sanatoriy_Karpaty.html' ?>
+                <a name="article_13_Karpaty_Train_Station"><?php include 'article_13_Karpaty_Train_Station.html' ?>
+                <a name="article_14_Mukachevo"><?php include 'article_14_Mukachevo.html' ?>
+                <a name="article_15_Chizay"><?php include 'article_15_Chizay.html' ?>
+                <a name="article_16_Road"><?php include 'article_16_Road.html' ?>
+			</article>
+        </article>
+    </BODY>
+</HTML>
