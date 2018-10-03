76d81213d64167b62ea2694112234481fc3481a0
Denis Scholokov <scholokov@users.noreply.github.com>
2018-10-03 20:59:27 +0300

    Buki

---
+++ Travels/20180915_Buki/article.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+    <HEAD>
+        <TITLE></TITLE>
+    </HEAD>
+    <BODY>
+        <article>
+            <header>
+                <a name="header"></a><?php include 'article header.html' ?>
+            </header>
+			<article>
+                <a name="article_01_Road"><?php include 'article_01_Road.html' ?>
+                <a name="article_02_Buki"><?php include 'article_02_Buki.html' ?>
+                <a name="article_03_Road"><?php include 'article_03_Road.html' ?>
+                <a name="article_04_Bila_Tserkva"><?php include 'article_04_Bila_Tserkva.html' ?>
+                <a name="article_05_Road"><?php include 'article_05_Road.html' ?>
+			</article>
+        </article>
+    </BODY>
+</HTML>
