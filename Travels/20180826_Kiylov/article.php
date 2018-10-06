56e4cf330302dd734d4defdaa2223ce8eb9b3eb4
Denis Scholokov <scholokov@users.noreply.github.com>
2018-10-06 11:51:35 +0300

    kiylov

---
+++ Travels/20180826_Kiylov/article.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+    <HEAD>
+        <TITLE></TITLE>
+    </HEAD>
+    <BODY>
+        <article>
+            <header>
+                <a name="header"></a><?php include 'article header.html' ?>
+            </header>
+			<article>
+                <a name="article"><?php include 'article.html' ?>
+			</article>
+        </article>
+    </BODY>
+</HTML>
