27753564f01c6e686d5af816f4833d47731ae5d4
Denis Scholokov <scholokov@users.noreply.github.com>
2018-10-06 10:32:30 +0300

    template

--- Travels/!_20161023_Template/article.php
+++ Travels/!_20161023_Template/article.php
@@ -19,3 +19,23 @@
         </article>
     </BODY>
 </HTML>
+
+
+
+<!DOCTYPE html>
+    <HEAD>
+        <TITLE></TITLE>
+    </HEAD>
+    <BODY>
+        <article>
+            <header>
+                <a name="header"></a><?php include 'article header.html' ?>
+            </header>
+			<article>
+                <a name="article_01_Road"><?php include 'article_01_Road.html' ?>
+                <a name="article_02"><?php include 'article_02.html' ?>
+                <a name="article_03_Road"><?php include 'article_03_Road.html' ?>
+			</article>
+        </article>
+    </BODY>
+</HTML>
