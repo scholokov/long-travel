89509b1560d8e529bd0d4e123beb0bb35f817e11
Denis Scholokov <scholokov@users.noreply.github.com>
2018-10-06 14:30:37 +0300

    bucha

--- Travels/!_20161023_Template/article.php
+++ Travels/!_20161023_Template/article.php
@@ -39,3 +39,20 @@
         </article>
     </BODY>
 </HTML>
+
+
+<!DOCTYPE html>
+    <HEAD>
+        <TITLE></TITLE>
+    </HEAD>
+    <BODY>
+        <article>
+            <header>
+                <a name="header"></a><?php include 'article header.html' ?>
+            </header>
+            <article>
+                <a name="article"><?php include 'article.html' ?>
+            </article>
+        </article>
+    </BODY>
+</HTML>
