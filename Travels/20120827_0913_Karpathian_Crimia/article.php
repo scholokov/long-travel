9a4bb0e98f241a9db22977fc00e9c4396a2aad7e
Denis Scholokov <scholokov@users.noreply.github.com>
2018-10-16 20:34:53 +0300

    crimea

--- Travels/20120827_0913_Karpathian_Crimia/article.php
+++ Travels/20120827_0913_Karpathian_Crimia/article.php
@@ -9,50 +9,22 @@
             </header>
 			<article>
                 <a name="article_01_Road"><?php include 'article_01_Road.html' ?>
-                <a name="article_02_Piryatin"><?php include 'article_02_Piryatin.html' ?>
-                <a name="article_03_Piryatin_Island"><?php include 'article_03_Piryatin_Island.html' ?>
-                <a name="article_04_Piryatin_Oil_Station"><?php include 'article_04_Piryatin_Oil_Station.html' ?>
-                <a name="article_05_Road"><?php include 'article_05_Road.html' ?>
-                <a name="article_06_Mirgorod"><?php include 'article_06_Mirgorod.html' ?>
-                <a name="article_07_Road"><?php include 'article_07_Road.html' ?>
-			</article>
-        </article>
-    </BODY>
-</HTML>
-
-
-
-<!DOCTYPE html>
-    <HEAD>
-        <TITLE></TITLE>
-    </HEAD>
-    <BODY>
-        <article>
-            <header>
-                <a name="header"></a><?php include 'article header.html' ?>
-            </header>
-			<article>
-                <a name="article_01_Road"><?php include 'article_01_Road.html' ?>
-                <a name="article_02"><?php include 'article_02.html' ?>
+                <a name="article_02_Karina"><?php include 'article_02_Karina.html' ?>
                 <a name="article_03_Road"><?php include 'article_03_Road.html' ?>
+                <a name="article_04_Hotel"><?php include 'article_04_Hotel.html' ?>
+                <a name="article_05_Road"><?php include 'article_05_Road.html' ?>
+                <a name="article_06_Hotel_Nikolaev"><?php include 'article_06_Hotel_Nikolaev.html' ?>
+                <a name="article_07_Road"><?php include 'article_07_Road.html' ?>
+                <a name="article_08_Ribache"><?php include 'article_08_Ribache.html' ?>
+                <a name="article_09_Crimea"><?php include 'article_09_Crimea.html' ?>
+                <a name="article_10_Beach"><?php include 'article_10_Beach.html' ?>
+                <a name="article_11_Alushta"><?php include 'article_11_Alushta.html' ?>
+                <a name="article_12_Yalta"><?php include 'article_12_Yalta.html' ?>
+                <a name="article_13_Azov_Sea"><?php include 'article_13_Azov_Sea.html' ?>
+                <a name="article_14_Road"><?php include 'article_14_Road.html' ?>
+                <a name="article_15_Hotel"><?php include 'article_15_Hotel.html' ?>
+                <a name="article_16_Road"><?php include 'article_16_Road.html' ?>          
 			</article>
-        </article>
-    </BODY>
-</HTML>
-
-
-<!DOCTYPE html>
-    <HEAD>
-        <TITLE></TITLE>
-    </HEAD>
-    <BODY>
-        <article>
-            <header>
-                <a name="header"></a><?php include 'article header.html' ?>
-            </header>
-            <article>
-                <a name="article"><?php include 'article.html' ?>
-            </article>
         </article>
     </BODY>
 </HTML>
